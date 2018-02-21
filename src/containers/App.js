0be4f9ed2f8bbecf674f77e7126e5457dc69cc00
github username <juliannne@gmail.com>
2018-02-21 10:53:35 -0500

    Add constructor, componentDidMount, import Posts

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import Posts from './Posts'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/posts')
+      .then(response => response.json())
+      .then(posts => this.setState({posts}))
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Posts</h1>
+          <Posts posts={this.state.posts} />
         </header>
       </div>
     );
