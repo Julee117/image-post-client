70aec04ef7b0bbb95743f17b00e659f5017d47bd
github username <juliannne@gmail.com>
2018-02-21 14:04:34 -0500

    Add key to PostCard component

--- src/containers/Posts.js
+++ src/containers/Posts.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PostCard from '../components/PostCard';
+import PostForm from './PostForm';
 
 const Posts = (props) => (
   <div className="PostsContainer">
     <h1>Posts</h1>
-    {props.posts.map(post => <PostCard post={post} />)}
+    {props.posts.map(post => <PostCard key={post.id} post={post} />)}
   </div>
 )
 
