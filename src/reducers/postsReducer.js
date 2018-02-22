45593e87386713b1fd6008a61298ea4be90cc31e
github username <juliannne@gmail.com>
2018-02-22 15:18:52 -0500

    Add case CREATE_POST

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch(action.type) {
     case 'GET_POSTS':
       return action.posts;
+    case 'CREATE_POST':
+      return state.concat(action.post);
     default:
       return state;
   }
