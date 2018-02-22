5220ff24a51f4998611c21c7683e5fb35dfb7802
github username <juliannne@gmail.com>
2018-02-22 13:49:04 -0500

    Create rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import postsReducer from './postsReducer';
+import postFormReducer from './postFormReducer';
 
 const rootReducer = combineReducers({
-  posts: postsReducer
+  posts: postsReducer,
+  postFormData: postFormReducer
 })
 
 export default rootReducer;
