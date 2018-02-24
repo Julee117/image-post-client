2225ec2ce0d443e1fded1619a86ba33846533854
github username <juliannne@gmail.com>
2018-02-24 02:45:09 -0500

    Add switch and routes for PostsPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Posts from './containers/Posts';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PostsPage from './containers/PostsPage';
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 
 class App extends Component {
 
@@ -10,7 +10,14 @@ class App extends Component {
       <div className="App">
         <Router>
           <div>
-            <Route path="/" component={Posts} />
+            <div style={{ paddingBottom: '10px', marginBottom: '12px' }}>
+              <NavLink style={{ marginRight: '10px' }} to="/">Home</NavLink>
+              <NavLink style={{ marginRight: '10px' }} to="/posts/new">New post</NavLink>
+            </div>
+            <Switch>
+              <Route path="/posts" component={PostsPage} />
+              <Route path="/" component={PostsPage} />
+            </Switch>
           </div>
         </Router>
       </div>
