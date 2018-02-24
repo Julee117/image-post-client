626483ca8c56f422f7bd91d467065ddab10e2d3f
github username <juliannne@gmail.com>
2018-02-24 15:57:46 -0500

    Add constructor, handleChange functions

--- src/containers/CommentForm.js
+++ src/containers/CommentForm.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 
 class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: ""
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      content: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="commentForm">
         <h4>Write a comment</h4>
         <form>
           <div>
-            <input
+            <textarea
               type="text"
+              onChange={this.handleChange}
               name="content"
+              value={this.state.content}
             />
           </div>
           <button type="submit">Post</button>
