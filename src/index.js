34d221a546c111a7cd027cccd510d47e8a571ceb
github username <juliannne@gmail.com>
2018-02-23 20:19:24 -0500

    Import bootstrap

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const store = createStore(
   rootReducer,
