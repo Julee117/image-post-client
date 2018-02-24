51f8a1f0545c11cf53a8e12a0f4be428b57e68e2
github username <juliannne@gmail.com>
2018-02-24 01:30:12 -0500

    Made posts into a presentation component, moved it to components folder

---
+++ src/components/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Posts = ({ posts }) => {
+  const renderPosts = posts.map(post =>
+    <Link key={post.id} to={`/posts/${post.id}`}><img className="PostImage" src={post.image_url} alt={post.title} /></Link>
+  );
+
+  return(
+    <div className="PostsContainer">
+      {renderPosts}
+    </div>
+  )
+}
+
+export default Posts;
