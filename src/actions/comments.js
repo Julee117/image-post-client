4430ee9f79af2fdf5f5ab4f2e53d48c2e7bf3007
github username <juliannne@gmail.com>
2018-02-25 00:59:12 -0500

    Add createComment

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -7,11 +7,31 @@ const setComments = comments => {
   }
 }
 
+const addComment = comment => {
+  return {
+    type: 'CREATE_COMMENT',
+    comment
+  }
+}
+
 export const getComments = () => {
   return dispatch => {
     return fetch(`${API_URL}/comments`)
       .then(response => response.json())
       .then(comments => dispatch(setComments(comments)))
       .catch(error => console.log(error));
+  }
+}
+
+export const createComment = comment => {
+  return dispatch => {
+    return fetch(`${API_URL}/comments`, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({comment: comment})
+    })
+      .then(response => response.json())
+      .then(comment => dispatch(addComment(comment)))
+      .catch(error => console.log(error))
   }
 }
