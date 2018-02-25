7139cf3dcc5c8d3d1ee92a9b170e1f90f460b4e1
github username <juliannne@gmail.com>
2018-02-25 01:33:39 -0500

    Add CommentForm, CommentsList

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import CommentForm from '../containers/CommentForm';
+import CommentsList from '../containers/CommentsList';
 
 const PostShow = ({ post }) =>
   <div className="postCard">
     <h3 className="titleHeader">{post.title}</h3>
     <img className="showImage" src={post.image_url} alt={post.title} />
     <p className="imageContent">{post.content}</p>
+    <div>
+      <CommentForm post={post}/>
+      <CommentsList post={post}/>
+    </div>
   </div>;
 
 const mapStateToProps = (state, ownProps) => {
