16dd8f4a1baeaebc7f7011857797244adba1fdee
github username <juliannne@gmail.com>
2018-02-25 01:00:32 -0500

    Add commentsReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import postsReducer from './postsReducer';
 import postFormReducer from './postFormReducer';
+import commentsReducer from './commentsReducer';
 
 const rootReducer = combineReducers({
   posts: postsReducer,
-  postFormData: postFormReducer
+  postFormData: postFormReducer,
+  comments: commentsReducer
 })
 
 export default rootReducer;
