72af2f8870b1db0d75e5442ba9ab017551636aa8
github username <juliannne@gmail.com>
2018-02-25 15:05:46 -0500

    Add handleOnClick and mapDispatchToProps

--- src/containers/CommentShow.js
+++ src/containers/CommentShow.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { deleteComment } from '../actions/comments';
 
 class CommentShow extends Component {
 
+  handleOnClick = () => {
+    this.props.deleteComment(this.props.comment)
+  }
+
   render() {
+
     return (
       <div className="comment">
         <p key={this.props.comment.id}>
           {this.props.comment.content}
         </p>
+        <button onClick={this.handleOnClick}>Delete</button>
       </div>
     )
   }
 }
 
-export default CommentShow;
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    deleteComment
+  }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(CommentShow);
