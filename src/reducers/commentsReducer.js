28cbcfbb343be2df0f7139988031a756179ad653
github username <juliannne@gmail.com>
2018-02-25 00:59:56 -0500

    Add case CREATE_COMMENT

--- src/reducers/commentsReducer.js
+++ src/reducers/commentsReducer.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch(action.type) {
     case 'GET_COMMENTS':
       return action.comments;
+    case 'CREATE_COMMENT':
+      return state.concat(action.comment);
     default:
       return state;
   }
