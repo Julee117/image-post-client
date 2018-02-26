e25720209ff1412a78b555f0d20ea257dbf5d671
github username <juliannne@gmail.com>
2018-02-25 23:20:39 -0500

    Add uploadFile function

---
+++ src/containers/UploadImage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+import sha1 from 'sha1';
+
+class UploadImage extends Component {
+  uploadFile = files => {
+    const image = files[0]
+    const cloudName = 'du8aeeumk'
+    const url = 'https://api/cloudinary.com/v1_1/'+cloudName+'/image/upload'
+    const timestamp = Date.now()/1000
+    const uploadPreset = 'kdbpev7a'
+    const paramsStr = 'timestamp='+timestamp+'&upload_preset='+uploadPreset+'WLuokBLxM5t5AL2uv_Kj6EQLBlc'
+    const signature = sha1(paramsStr)
+
+    const params = {
+      'api_key': '965958725763533',
+      'timestamp': timestamp,
+      'upload_preset': uploadPreset,
+      'signature': signature
+    }
+  }
+}
+
+export default UploadImage;
