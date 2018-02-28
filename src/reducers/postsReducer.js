406b5cf769f9d33bd7c4b4d5b099ce169b7e74a5
github username <juliannne@gmail.com>
2018-02-28 10:16:18 -0500

    Modify case ADD_LIKES

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -4,6 +4,14 @@ export default (state = [], action) => {
       return action.posts;
     case 'CREATE_POST':
       return state.concat(action.post);
+    case 'ADD_LIKES':
+      let index = state.findIndex(post => post.id === action.data.id);
+      let post = state[index];
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, post, {likes: post.likes += 1}),
+        ...state.slice(index+1)
+      ];
     default:
       return state;
   }
