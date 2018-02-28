7b6515bcace1e1549e8543f1ef49d1f0504e0a77
github username <juliannne@gmail.com>
2018-02-28 14:31:22 -0500

    Add css to Posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,14 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Posts = ({ posts }) => {
   const renderPosts = posts.map(post =>
-    <Link key={post.id} to={`/posts/${post.id}`}><img className="postImage" src={post.image_url} alt={post.title} /></Link>
+
+      <Link key={post.id} to={`/posts/${post.id}`}><img className="postImage img-responsive" src={post.image_url} alt={post.title} /></Link>
+
   );
 
   return(
     <div>
       <h3 className="postHeader">Funny viral images</h3>
-      <div className="postsContainer">
-        {renderPosts}
+      <div className="postsContainer container">
+        <div className="row">
+          <div className="col-lg-12 col-md-4 col-sm-6">
+            {renderPosts}
+          </div>
+        </div>
       </div>
     </div>
   )
