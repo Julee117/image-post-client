9229ccd97f0d491fd30954e00df51941928301b1
github username <juliannne@gmail.com>
2018-02-28 10:11:08 -0500

    Remove postFormReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import {combineReducers} from 'redux';
 import postsReducer from './postsReducer';
-import postFormReducer from './postFormReducer';
 import commentsReducer from './commentsReducer';
 
 const rootReducer = combineReducers({
   posts: postsReducer,
-  postFormData: postFormReducer,
   comments: commentsReducer
 })
 
