717e6152b28671f21be963239abcdb3bd0125a9f
github username <juliannne@gmail.com>
2018-02-28 22:42:00 -0500

    Remove spacing, add || operator

--- src/components/PostLikes.js
+++ src/components/PostLikes.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const PostLikes = ({post, addLikes}) =>
   <div>
-    <p>
-      <i onClick={() => addLikes(post)} className="fa fa-thumbs-o-up fa-2x" aria-hidden="true"></i>
+    <p className="icons">
+      <i onClick={() => addLikes(post)} className="fa fa-thumbs-o-up fa-2x thumbs" aria-hidden="true"></i>
       <span> Like</span>
+      <span className="likes">{post.likes === null || post.likes === 0 ? "" : <i className="fa fa-smile-o smile" aria-hidden="true"> {post.likes} likes</i>}</span>
     </p>
-    <p>{post.likes === null ? "" : <i className="fa fa-smile-o" aria-hidden="true"> {post.likes}</i>}</p>
   </div>
 
 export default PostLikes;
