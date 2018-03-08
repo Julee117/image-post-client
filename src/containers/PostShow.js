31ee2c589c7e5c05c7018b7d005e89a71f717a9a
github username <juliannne@gmail.com>
2018-03-07 23:08:44 -0500

    Modify PostShow to class

--- src/containers/PostShow.js
+++ src/containers/PostShow.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import CommentForm from './CommentForm';
@@ -7,20 +7,27 @@ import PostLikes from '../components/PostLikes';
 import { addLikes } from '../actions/posts';
 import PostHeader from '../components/PostHeader';
 
-const PostShow = ({ post, addLikes }) =>
-  <div className="postCard col-lg-6">
-    <PostHeader post={post}/>
-    <img className="showImage" src={post.image_url} alt={post.title} />
-    <div className="imageContent">
-      <p>{post.content}</p>
-      <p className="line"></p>
-      <PostLikes post={post} addLikes={addLikes}/>
-    </div>
-    <div>
-      <CommentForm post={post}/>
-      <CommentsList post={post}/>
-    </div>
-  </div>;
+class PostShow extends Component {
+  render() {
+    const { post, addLikes } = this.props;
+
+    return (
+      <div className="postCard col-lg-6">
+        <PostHeader post={post}/>
+        <img className="showImage" src={post.image_url} alt={post.title} />
+        <div className="imageContent">
+          <p>{post.content}</p>
+          <p className="line"></p>
+          <PostLikes post={post} addLikes={addLikes} />
+        </div>
+        <div>
+          <CommentForm post={post} />
+          <CommentsList post={post} />
+        </div>
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   const post = state.posts.find(post => post.id === +ownProps.match.params.postId)
